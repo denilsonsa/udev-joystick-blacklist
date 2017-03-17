da5f9215b2b789e56391e8898db1d3244bf4a584
Denilson Sá Maia <denilsonsa@gmail.com>
2017-03-17 08:45:24 +0100

    Adding [T3] 2.4GHz and IR Air Mouse Remote Control.
    Closes #27

--- generate_rules.py
+++ generate_rules.py
@@ -84,6 +84,8 @@ DEVICES = [
     ('09da', 'f613'),  # A4 Tech Co., Ltd Bloody V2 mouse
     ('09da', 'f624'),  # A4 Tech Co., Ltd Bloody B120 Keyboard
 
+    ('1d57', 'ad03'),  # [T3] 2.4GHz and IR Air Mouse Remote Control
+
     ('1e7d', '2e4a'),  # Roccat Tyon Mouse
 
     ('20a0', '422d'),  # Winkeyless.kr Keyboards
